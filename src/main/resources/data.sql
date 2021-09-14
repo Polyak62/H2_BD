95a7dcc96de6b144537f8d7999e85e0775502577
Polyak62 <anatolich1997@gmail.com>
2021-09-14 15:40:53 +0300

    First commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO AUTHOR (name) VALUES
+  ('Dan'),
+  ('Jen'),
+  ('Bella'),
+   ('Bob'),
+    ('John');
+
+
+INSERT INTO BOOK (name, author_id) VALUES
+  ('Kolobok', 1),
+  ('Azbuka', 2),
+  ('Wolf and seven goatlings', 3),
+   ('Idiot', 4),
+    ('Stalker', 5);
+
+
+INSERT INTO GENRE (name,book_id) VALUES
+  ('detective', 1),
+  ('comedy', 2),
+  ('drama', 3),
+   ('fantastic', 4),
+    ('horror', 5);
